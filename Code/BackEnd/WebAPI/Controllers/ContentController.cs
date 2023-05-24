7d29b8811d0d0dc222f81000a518a22e9db384e2
Kadir Berk Yağar <ykadirberk2@gmail.com>
2023-05-25 02:18:25 +0300

    Added "Find contents liked by user" functionality

--- Code/BackEnd/WebAPI/Controllers/ContentController.cs
+++ Code/BackEnd/WebAPI/Controllers/ContentController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using WebAPI.DataManagement;
 using WebAPI.Entities;
 
@@ -17,12 +18,22 @@ public class ContentController : ControllerBase {
     [HttpGet("GetById/{id}")]
     public Content GetById(int id) {
         var content = TempDatabase.ContentList.Where(t => t.contentId == id).SingleOrDefault();
+        return content;
+    }
+
+    [HttpGet("GetByUser/{Id}")]
+    public List<Content> FindByUserId(int Id) {
+        var content = TempDatabase.ContentList.Where(t => t.authorId == Id).ToList<Content>();
         return content; 
     }
 
-    [HttpGet("GetByUser/{userId}")]
-    public List<Content> FindByUserId(int userId) {
-        var content = TempDatabase.ContentList.Where(t => t.authorId == userId).ToList<Content>();
-        return content; 
+    [HttpGet("FindLikedByUser")]
+    public List<Content> FindLikedByUser([FromQuery, BindRequired] int Id) {
+        var likedContentIds = TempDatabase.LikeList
+                .Where(t => t.userId == Id).ToList<Like>()
+                .Select(t => t.likedContentId)
+                .ToList<int>();
+        var contents = TempDatabase.ContentList.Where(t => likedContentIds.Contains(t.contentId)).ToList<Content>();
+        return contents; 
     }
 }
