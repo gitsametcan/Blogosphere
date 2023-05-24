18e0dc2d03b2303e7932363523a479ff18c76aa8
Kadir Berk Yağar <ykadirberk2@gmail.com>
2023-05-24 14:43:17 +0300

    register user call

--- Code/BackEnd/WebAPI/Controllers/UserController.cs
+++ Code/BackEnd/WebAPI/Controllers/UserController.cs
@@ -19,4 +19,29 @@ public class UserController : ControllerBase {
         var user = TempDatabase.UserList.Where(t => t.userId == id).SingleOrDefault();
         return user; 
     }
+
+    [HttpGet("GetByUsername/{username}")]
+    public User GetByUsername(string username) {
+        var user = TempDatabase.UserList.Where(t => t.username.Equals(username)).SingleOrDefault();
+        return user;
+    }
+
+    [HttpGet("GetByEmail/{email}")]
+    public User GetByEmail(string email) {
+        var user = TempDatabase.UserList.Where(t => t.email.Equals(email)).SingleOrDefault();
+        return user;
+    }
+
+    [HttpPost("RegisterUser")]
+    public IActionResult RegisterUser([FromBody] User user) {
+        var tempuser = TempDatabase.UserList
+            .Where(t => t.username == user.username || t.email == user.email || t.userId == user.userId)
+            .SingleOrDefault();
+        if (tempuser is not null) {
+            return BadRequest();
+        }
+        TempDatabase.UserList.Add(user);
+        return Ok();
+    }
+
 }
