cf8e9197c00e080979a36fa01dde4fa39e0174bb
Samet CAN <108118230+gitsametcan@users.noreply.github.com>
2023-05-25 14:54:28 +0300

    Profil-page component finished

--- Code/Blogosphere/src/app/app.module.ts
+++ Code/Blogosphere/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
+import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
