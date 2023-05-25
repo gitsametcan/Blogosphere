8313bbb51a9eba3ffba1d25dc5b7121fe0738291
alikosar <ali.kosarr@hotmail.com>
2023-05-25 14:52:03 +0300

    Content Details düzenlendi, Routering sağlandı

--- Code/Blogosphere/src/app/app-routing.module.ts
+++ Code/Blogosphere/src/app/app-routing.module.ts
@@ -2,16 +2,24 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { ContentDetailsComponent } from './content-details/content-details.component';
+import { ListCardsComponent } from './list-cards/list-cards.component';
+import { ProfilPageComponent } from './profil-page/profil-page.component';
 
-const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: '', redirectTo: '/login', pathMatch: 'full' }, // Redirect empty path to login
-  // other routes
+
+const appRoute: Routes =[
+  {path: '#', redirectTo:'home', pathMatch:'full'},
+  {path: '', redirectTo:'home', pathMatch:'full'},
+  {path: 'content-details', component:ContentDetailsComponent},
+  {path: 'home', component:ListCardsComponent},
+  {path: 'profil', component:ProfilPageComponent},
+  {path: 'login',component:LoginComponent},
+  {path: 'register', component:RegisterComponent}
+
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoute)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- Code/Blogosphere/src/app/app.module.ts
+++ Code/Blogosphere/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,6 +14,7 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -30,7 +32,7 @@ import { FormsModule } from '@angular/forms';
     AppRoutingModule,
     NgbModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
