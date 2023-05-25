672cc006cb8ab2740afbaa30d07db94cee2f7ce8
Kadir Berk Yağar <ykadirberk2@gmail.com>
2023-05-25 14:05:15 +0300

    Program now allows cross-origin requests

--- Code/BackEnd/WebAPI/Program.cs
+++ Code/BackEnd/WebAPI/Program.cs
@@ -1,11 +1,26 @@
 var builder = WebApplication.CreateBuilder(args);
 
+string allowAllPolicy = "_AllowAll";
+/*
+builder.Services.AddCors(options => {
+        options.AddPolicy(name: allowAllPolicy, builder => {
+                builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials();
+            }
+        );
+    }
+);*/
+
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
 
 var app = builder.Build();
 
@@ -17,6 +32,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+//app.UseCors(allowAllPolicy);
+
+app.UseCors(options => options.AllowAnyOrigin());
 
 app.UseAuthorization();
 
