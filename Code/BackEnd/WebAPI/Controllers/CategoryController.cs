233ce99f564b411e72b898e4705cc1194be643d7
Kadir Berk Yağar <ykadirberk2@gmail.com>
2023-05-27 16:59:31 +0300

    Category operations added

--- Code/BackEnd/WebAPI/Controllers/CategoryController.cs
+++ Code/BackEnd/WebAPI/Controllers/CategoryController.cs
@@ -16,13 +16,35 @@ public class CategoryController : ControllerBase {
 
     [HttpGet("GetById/{id}")]
     public Category? GetById(int id) {
-        var comment = TempDatabase.CategoryList.Where(book => book.categoryId == id).SingleOrDefault();
+        var comment = TempDatabase.CategoryList.Where(t => t.categoryId == id).SingleOrDefault();
         return comment; 
     }
 
     [HttpGet("FindByName/{name}")]
     public List<Category> FindByName(string name) {
-        var commentList = TempDatabase.CategoryList.Where(x => x.categoryTitle.Contains(name)).ToList<Category>();
+        var commentList = TempDatabase.CategoryList.Where(t => t.categoryTitle.Contains(name)).ToList<Category>();
         return commentList;
+    }
+
+    [HttpPost("NewCategory")]
+    public IActionResult NewCategory([FromBody] Category category) {
+        TempDatabase.CategoryList.Add(category);
+        return Ok();
+    }
+
+    [HttpPut("UpdateCategory")]
+    public IActionResult UpdateCategory([FromBody] Category category) {
+        TempDatabase.CategoryList.Add(category);
+        return Ok();
+    }
+
+    [HttpDelete("DeleteCategory/{categoryId}")]
+    public IActionResult DeleteCategory(int categoryId) {
+        var category = TempDatabase.CategoryList.SingleOrDefault(t => t.categoryId == categoryId);
+        if (category is null) {
+            return NotFound("Category with id " + categoryId + " not found.");
+        }
+        TempDatabase.CategoryList.Remove(category);
+        return Ok();
     }
 }
