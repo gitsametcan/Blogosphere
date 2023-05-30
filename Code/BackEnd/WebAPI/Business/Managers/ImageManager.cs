d2ab62161b543322c38a851e5126eb15d7612bc0
Kadir Berk Yağar <ykadirberk2@gmail.com>
2023-05-30 12:00:58 +0300

    GetImage works fine, Hashing added for uploaded name.

--- Code/BackEnd/WebAPI/Business/Managers/ImageManager.cs
+++ Code/BackEnd/WebAPI/Business/Managers/ImageManager.cs
@@ -1,3 +1,5 @@
+using System.Security.Cryptography;
+using System.Text;
 using Microsoft.AspNetCore.Mvc;
 using WebAPI.Business.Services;
 using WebAPI.Core.Result;
@@ -11,17 +13,28 @@ public class ImageManager : IImageService {
         return new PhysicalFileResult(System.IO.Directory.GetCurrentDirectory() + "\\Assets\\" + FilePath, "image/jpeg");
     }
     public Result UploadImage(IFormFile file) {
-        string fName = file.FileName;
-        string path = Path.Combine(System.IO.Directory.GetCurrentDirectory(), "Assets/" + file.FileName);
+        string fName = StringToSHA256(file.FileName);
+        string path = System.IO.Directory.GetCurrentDirectory() + "\\Assets\\" + fName + ".jpg";
         using (var stream = new FileStream(path, FileMode.Create))
         {
             file.CopyTo(stream);
         }
-        return new Result(true, "File successfully uploaded: " + file.FileName);
+        return new Result(true, fName + ".jpg");
     }
     public Result DeleteImage(string FileName) {
         File.Delete(System.IO.Directory.GetCurrentDirectory() + "\\Assets\\" + FileName);
         return new Result(true, "File successfully deleted: " + FileName);
     }
+
+    private string StringToSHA256(string key)  {
+            using (SHA256 sha256Hash = SHA256.Create()) {  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(key));
+                StringBuilder builder = new StringBuilder();
+                foreach (var item in bytes) {
+                    builder.Append(item.ToString("x2"));
+                }
+                return builder.ToString();  
+            }  
+        } 
 
 }
