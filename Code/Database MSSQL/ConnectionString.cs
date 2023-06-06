5a56e3162cf8f2f9b12f9c33eee0d5eaf63a04fa
Kadir Berk Yağar <ykadirberk2@gmail.com>
2023-06-06 16:21:07 +0300

    Database Requirements

---
+++ Code/Database MSSQL/ConnectionString.cs
@@ -0,0 +1,5 @@
+namespace WebAPI.Models.Utils;
+
+public static class ConnectionString {
+    public static string ServerString = "Server=KADIR-BERK\\SQLEXPRESS;Database=BLOGOSPHERE;Trusted_Connection=true;TrustServerCertificate=True;";
+}
